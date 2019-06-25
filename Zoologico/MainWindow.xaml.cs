a9ebb05adb3219a980c1f33318470c51632485ed
hp <hp@LAPTOP-RTAH6NE8>
2019-06-25 10:23:03 -0600

    Funcion llenar listado de animales mediante base de datps

--- Zoologico/MainWindow.xaml.cs
+++ Zoologico/MainWindow.xaml.cs
@@ -34,6 +34,9 @@ namespace Zoologico
             //ZoologicoConnectionString
             string connectionString = ConfigurationManager.ConnectionStrings["Zoologico.Properties.Settings.ZoologicoConnectionString"].ConnectionString;
             sqlConnection = new SqlConnection(connectionString);
+
+            MostrarZoologico();
+            MostrarAnimales();
             
         }
        
@@ -44,7 +47,7 @@ namespace Zoologico
             {
                  MessageBox.Show("Entrando al try");
                 //El query a realizar en la base de datos
-                string query = "SELECT * FROM ZOO.Animal";
+                string query = "SELECT * FROM ZOO.Zoologico";
 
                 //sqlDataAdapater es una intrefaz entre las tablas y los objetos utilizables de C#
                 SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query,sqlConnection);
@@ -55,12 +58,11 @@ namespace Zoologico
                     //Llenar el objeto de tipo DataTable
                     sqlDataAdapter.Fill(tablaZoologico);
                     //Desplegar el campo que se desea mandar a llamar|| para el usuario
-                    lbZoologicos.DisplayMemberPath = "nombre";
+                    lbZoologicos.DisplayMemberPath = "ciudad";
                     //valor entregado|| Para el prorgramadoe
                     lbZoologicos.SelectedValuePath = "id";
                     //Referencia de los datos para el listbox(popular)
                     lbZoologicos.ItemsSource = tablaZoologico.DefaultView;
-                    MostrarZoologico();
                     MessageBox.Show("Exitosa");
 
                 }
@@ -68,8 +70,64 @@ namespace Zoologico
             catch(Exception e)
             {
 
-                MessageBox.Show(e.ToString());
+                MessageBox.Show(e.ToString(),e.Message);
             }
+        }
+        private void MostrarAnimalesZoologico()
+        {
+            try
+            {
+                string query = @"SELECT * FROM ZOO.Animal a INNER JOIN ZOO.AnimalZoologico b
+                               ON a.id=b.idAnimal WHERE b.idZoologico=@zooId";
+                //Comando SQL
+                SqlCommand sqlcommand= new SqlCommand(query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlcommand);
+                using (sqlDataAdapter)
+                {
+                    //Reemplazar el valor del parametro del query 
+                    sqlcommand.Parameters.AddWithValue("@zooId", lbZoologicos.SelectedValue);
+                    DataTable tablaAnimalZoologico = new DataTable();
+
+                    sqlDataAdapter.Fill(tablaAnimalZoologico);
+                    lbAnimalesZoologicos.DisplayMemberPath = "nombre";
+                    lbAnimalesZoologicos.SelectedValuePath = "id";
+                    lbAnimalesZoologicos.ItemsSource = tablaAnimalZoologico.DefaultView;
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString(),e.Message);
+            }
+        }
+        private void MostrarAnimales()
+        {
+            try
+            {
+                string query = "SELECT * FROM ZOO.Animal";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+                using (sqlDataAdapter)
+                {
+                    DataTable tablaAnimal = new DataTable();
+                    sqlDataAdapter.Fill(tablaAnimal);
+                    lbAnimales.DisplayMemberPath = "nombre";
+                    lbAnimales.SelectedValue = "id";
+                    lbAnimales.ItemsSource = tablaAnimal.DefaultView;
+
+                }
+
+
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        private void LbZoologicos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MostrarAnimalesZoologico();
+
         }
     }
 }
